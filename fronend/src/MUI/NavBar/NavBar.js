569e56e06aed3367af5f5241cbbe8304328b767c
Ziyi Zhang <ziyizhang@MacBook-Pro-2.lan1>
2022-05-20 20:07:52 -0400

    5-20

---
+++ fronend/src/MUI/NavBar/NavBar.js
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import CurrentMonthExpenses from '../../expenses/CurrentMonthExpenses';
+
+const ResponsiveAppBar = () => {
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Personal Ledger
+          </Typography>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Button
+                key="transactions"
+                sx={{ my: 2, color: 'white', display: 'block' }}
+                href="/expenses"
+              >
+                Transactions
+              </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
---
+++ fronend/src/User/UserCardList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import UserCard from './UserCard';
+
+function UserCardList({users}){
+
+    console.log("UserCardList","users=",users);
+
+    return (
+        <div>
+            {users.map(user =>(
+                <UserCard 
+                key={user.username}
+                    username={user.username}
+                    firstName={user.first_name}
+                    lastName={user.last_name}
+                    email={user.email}
+                    />
+            ))}
+        </div>
+    )
+
+}
+
+export default UserCardList;
